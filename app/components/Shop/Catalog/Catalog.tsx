83b22b45cc0d387665955ba89ec80f6a25655ac9
SashaJozwiak <wmdz88@gmail.com>
2024-04-09 21:53:03 +0300

    fix: some styles for shop

--- app/components/Shop/Catalog/Catalog.tsx
+++ app/components/Shop/Catalog/Catalog.tsx
@@ -12,26 +12,31 @@ export default async function Catalog() {
 
     const items: any = await getAllPg();
 
-
-    //console.log(items)
-
     return (
         <div className={s.cat} >
             {
                 items.map((item: any) => {
+                    console.log(item.img_link)
                     return (
                         <div key={item.art} className={s.item}>
-                            {/* <Image src={item.image}
-                                className='h-48 mx-auto my-0'
-                                width={180} height={180}
+                            <Image src={`/products_img/${item.img_link}.webp`}
+                                className='h-48 mx-auto my-0 rounded-lg'
+                                width={260} height={260}
                                 //style={{ borderRadius: "20px", margin: "10px auto" }}
-                                alt={`${item.tlte} image`}
-                            /> */}
-                            {item.title}
-                            <p>{Math.ceil(item.price)}</p>
-                            <p>{Math.ceil(item.price_2)}</p>
-                            <p>{Math.ceil(item.price_3)}</p>
-                            <div>{item.bonuses}</div>
+                                alt={`${item.title} image`}
+                            />
+                            <h3 className='w-full text-2xl text-slate-600'>{item.title}</h3>
+                            <div className='flex w-full justify-around'>
+                                <p className='px-1 block bg-slate-200 rounded-md text-2xl text-slate-800'>{Math.ceil(item.price)} ₽</p>
+                                <p className='px-1 block bg-slate-100 rounded-md text-xl text-slate-500'>{Math.ceil(item.price_2)} ₽</p>
+                                <p className='px-1 block bg-slate-100 rounded-md text-xl text-slate-500'>{Math.ceil(item.price_3)} ₽</p>
+                            </div>
+
+                            <div className='flex w-full justify-around'>
+                                <button className='bg-slate-600 border py-1 px-4 mb-2 rounded-md text-slate-200'>В корзину</button>
+                                <div className='text-slate-800 mt-2'>Бонусы: {item.bonuses}₽</div>
+                            </div>
+
                         </div>
                     )
                 })
